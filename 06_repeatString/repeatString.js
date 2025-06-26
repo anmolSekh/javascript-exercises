d0a1d8fbfdec9565508cadb3ad01c9990ec80db7
Anmol <anmol1314@hotmail.com>
2025-06-26 03:03:54 -0700

    finished 1-6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count < 0) {
+        return 'ERROR';
+    }
+    let newstr = "";
+    for (i = 0; i < count; i++) {
+        newstr += str;
+    }
+    return newstr;
 };
 
 // Do not edit below this line
