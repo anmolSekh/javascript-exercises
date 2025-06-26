d3ac4e3305b78c1551ec64225559563bf881a9e0
Anmol <anmol1314@hotmail.com>
2025-06-26 04:12:18 -0700

    finished 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverse = ""
+    for( i = str.length-1; i >= 0; i--) {
+        reverse += str.charAt(i)
+    }
+    return reverse;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
+    let newarr = [];
+    for(i = 1; i < arr.length; i++) {
+        let index = arr.indexOf(arr[i]) + 1;
+        console.log(index)
+        arr.splice(index, 1);
+    }
+    console.log(arr)
+    return arr;
 };
 
 // Do not edit below this line
