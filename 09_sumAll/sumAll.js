20ac396906dcbe7578f3464660b7c1d2590f2d74
Anmol <anmol1314@hotmail.com>
2025-06-27 04:34:56 -0700

    commented todo stuff

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    //check if x or y are ints else return error
+    let sum = 0;
+    if(x < 0 | y <0) {
+        return 'ERROR';
+    }
+    if(x < y) {
+        for (i = x; i <= y; i++) {
+            sum += i;
+        }
+    } else {
+        for (i = y; i <= x; i++) {
+            sum += i;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
