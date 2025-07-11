dd8cc4044156482d761110375c7b847a33690e51
Anmol <anmol1314@hotmail.com>
2025-07-11 02:41:38 -0700

    improved bounds check

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(x, y) {
     //check if x or y are ints else return error
     let sum = 0;
     // if (typeof x == "")
-    if( Number.isInteger(x) == false | Number.isInteger(y) == false | x < 0 | y <0) {
+    if( Number.isInteger(x) == false | Number.isInteger(y) == false | x < 0 | y <0 | y < x) {
         return 'ERROR';
     }
     if(x < y) {
