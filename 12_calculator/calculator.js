1125ef03f5b0860ae9da5f0909f6157457b3049e
Anmol <anmol1314@hotmail.com>
2025-07-11 03:29:30 -0700

    completed 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return (x + y);
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return (x - y);
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+	let sum = 0;
+  for(i = 0; i < x.length; i++) {
+    sum = sum + x[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
 
+const multiply = function(x) {
+  let product = 1;
+  for(i = 0; i < x.length; i++) {
+    product = product * x[i];
+  }
+  return product;
+  // return x.reduce(a,b => a * b);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let z = x;
+	for(i = 1; i < y; i++) {
+    z = z * x;
+  }
+  return z;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let y = 1;
+	for(i = 1; i <= x; i++) {
+    y = y * i;
+  }
+  return y;
 };
 
 // Do not edit below this line
