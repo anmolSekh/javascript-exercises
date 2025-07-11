5975a9536304c1087055bd794f55c8b4f77514e4
Anmol <anmol1314@hotmail.com>
2025-07-11 03:47:31 -0700

    completed 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (strtest) {
+    let strlow = strtest.toLowerCase();
+    strlow = strlow.replace(/[^\w\s\']|_/g,"");
+    //  strlow.replace(/[^\w\s\']|_/g, "")
+    let str = strlow.replace(/\s/g, "");
+    console.log(str);
+    let i = 0;
+    let j = str.length - 1;
+    console.log(i + " == " + j);
+    while(i <j) {
+        if(str.charAt(i) != str.charAt(j)) {
+            return false;
+        }
+        // console.log(i + " == " + j);
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
