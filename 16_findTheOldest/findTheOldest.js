53879f71246aadb9b4a44aa5b34000cb76563559
Anmol <anmol1314@hotmail.com>
2025-07-12 23:20:00 -0700

    optimized 16 solution

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,21 +1,35 @@
 const findTheOldest = function(people) {
-    let oldest = undefined;
-    let oldestAge = 0;
-    const currentYear = new Date().getFullYear();
-    for(i = 0; i < people.length; i++) {
-        let age = 0;
-        if(people[i].yearOfDeath == undefined) {
-            age = currentYear - people[i].yearOfBirth;
-        } else {
-            age = people[i].yearOfDeath - people[i].yearOfBirth;
+    // let oldest = undefined;
+    // let oldestAge = 0;
+    // const currentYear = new Date().getFullYear();
+    // for(i = 0; i < people.length; i++) {
+    //     let age = 0;
+    //     if(people[i].yearOfDeath == undefined) {
+    //         age = currentYear - people[i].yearOfBirth;
+    //     } else {
+    //         age = people[i].yearOfDeath - people[i].yearOfBirth;
+    //     }
+    //     // console.log(people[i].yearOfDeath);
+    //     if(age > oldestAge) {
+    //         oldestAge = age;
+    //         oldest = people[i];
+    //     }
+    // }
+    
+    const yod = function(year) {
+        if(!year) {
+            year =  new Date().getFullYear();
         }
-        // console.log(people[i].yearOfDeath);
-        if(age > oldestAge) {
-            oldestAge = age;
-            oldest = people[i];
-        }
+
+        return year;
     }
-    return oldest;
+
+    return people.reduce((oldest, current) => {
+        
+        const oldestAge = (yod(oldest.yearOfDeath) - oldest.yearOfBirth);
+        const currentAge = (yod(current.yearOfDeath) - current.yearOfBirth);
+        return oldestAge < currentAge ? current : oldest;
+    })
 };
 
 // Do not edit below this line
