1ec131e4718f08af3be049dc4d67904ee66e610f
Anmol <anmol1314@hotmail.com>
2025-07-12 23:06:24 -0700

    optimized 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,9 +1,6 @@
 const getTheTitles = function(books) {
-    let titles = [];
-    for(i = 0; i < books.length; i++) {
-        titles.push(books[i].title)
-    }
-    return titles
+    
+    return books.map((book)=>book.title);
 };
 
 // Do not edit below this line
