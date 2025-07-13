bb4e6c5dcf205a936a26851f069a3fd0b53aa17f
Anmol <anmol1314@hotmail.com>
2025-07-12 23:03:09 -0700

    finished findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = undefined;
+    let oldestAge = 0;
+    const currentYear = new Date().getFullYear();
+    for(i = 0; i < people.length; i++) {
+        let age = 0;
+        if(people[i].yearOfDeath == undefined) {
+            age = currentYear - people[i].yearOfBirth;
+        } else {
+            age = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        // console.log(people[i].yearOfDeath);
+        if(age > oldestAge) {
+            oldestAge = age;
+            oldest = people[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
